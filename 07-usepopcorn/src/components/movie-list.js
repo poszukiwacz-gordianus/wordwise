2b9e656f3bff0c2787f708b18d174d5cfd29c00b
Gordianus <poszukiwacz.gordianus@gmail.com>
2024-08-19 23:54:05 +0200

    Component Composition

---
+++ 07-usepopcorn/src/components/movie-list.js
@@ -0,0 +1,10 @@
+import Movie from "./movie";
+export default function MovieList({ movies }) {
+  return (
+    <ul className="list">
+      {movies?.map((movie) => (
+        <Movie movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
+  );
+}
---
+++ 07-usepopcorn/src/components/watched-movie-list.js
@@ -0,0 +1,11 @@
+import WatchedMovie from "./watched-movie";
+
+export default function WatchedMovieList({ watched }) {
+  return (
+    <ul className="list">
+      {watched.map((movie) => (
+        <WatchedMovie movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
+  );
+}
