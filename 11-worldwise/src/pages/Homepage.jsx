a487b267f0ac2e91be4a82d8aad3a58782ed71e2
Gordianus <poszukiwacz.gordianus@gmail.com>
2024-08-24 10:58:06 +0200

    Set up Routes

---
+++ 11-worldwise/src/pages/Homepage.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
+function HomePage() {
+  return (
+    <div>
+      <PageNav />
+      <h1>WorldWise</h1>
+      <Link to={"/app"}>Go to the app</Link>
+    </div>
+  );
+}
+
+export default HomePage;
